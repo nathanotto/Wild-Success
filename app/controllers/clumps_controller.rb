6467fed1650edf55393f9ea94738372de9c403d8
Nathan Otto <gitemail@nathanotto.com>
2013-03-02 13:47:28 -0700

    Clumping working better

--- app/controllers/clumps_controller.rb
+++ app/controllers/clumps_controller.rb
@@ -14,7 +14,7 @@ class ClumpsController < ApplicationController
         t_id = @mission.id
         @clump  = Clump.find(params[:id])
         c = Collaborator.where(:user_id => current_user.id, :mission_id => @clump.mission_id).first
-         if (c.permission == 'creator' || c.permission == 'admin') || @clump.user_id == current_user.id then
+        if (c.permission == 'creator' || c.permission == 'admin') || @clump.user_id == current_user.id then
             # make the stickies un-clumped from this clump about to be destroyed
             @stickies = Sticky.where(:clump_id => @clump.id)
             if @stickies then @stickies.each do  |sticky|
@@ -24,7 +24,27 @@ class ClumpsController < ApplicationController
             end end
             @clump.destroy
             else
-             flash[:notice] = "Can't delete unless it's yours or you have admin permission."
+            flash[:notice] = "Can't delete unless it's yours or you have admin permission."
+        end
+        redirect_to mission_clumps_path(:mission_id => t_id, :kind => @clump.kind)
+    end
+    
+    def clear
+        @mission = Mission.find(params[:mission_id])
+        t_id = @mission.id
+        @clump  = Clump.find(params[:id])
+        c = Collaborator.where(:user_id => current_user.id, :mission_id => @clump.mission_id).first
+        if (c.permission == 'creator' || c.permission == 'admin' || c.permission == 'colleague') || @clump.user_id == current_user.id then
+            # make the stickies un-clumped from this clump
+            @stickies = Sticky.where(:clump_id => @clump.id)
+            if @stickies then @stickies.each do  |sticky|
+                sticky.clump_id = nil
+                @sticky = sticky
+                @sticky.save
+            end
+        end
+            else
+            flash[:notice] = "Can't clear this clump unless you are a collaborator."
         end
         redirect_to mission_clumps_path(:mission_id => t_id, :kind => @clump.kind)
     end
